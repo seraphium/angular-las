3039a80734a090c46aaa3c4ad390c4932462fdca
seraphium <viperking@163.com>
2017-06-20 11:44:32 +0800

    added support on karma ut framework

---
+++ src/app/1st.spec.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by zezhang on 2017/6/20.
+ */
+describe('1st test', () => {
+  it('true is true', () => expect(true).toBe(true));
+});
---
+++ src/app/2nd.spec.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by zezhang on 2017/6/20.
+ */
+describe('2nd test', () => {
+  it('true is true', () => expect(true).toBe(true));
+});
